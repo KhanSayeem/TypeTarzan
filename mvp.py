1136671e68a1b9899ec4b1e57553219f66239bff
KhanSayeem <khansayeem03@gmail.com>
2026-03-19 00:00:37 +0600

    Implement Road scroll logic

--- mvp.py
+++ mvp.py
@@ -2,25 +2,40 @@ import pygame
 from sys import exit
 
 pygame.init()
-screen = pygame.display.set_mode((1000,600))
+screen = pygame.display.set_mode((1000, 600))
 pygame.display.set_caption("TypeTarzan")
 clock = pygame.time.Clock()
+scroll = 0
+car_speed = 3 
+
 road_surface = pygame.image.load('assets/road.jpeg').convert()
-road_surface = pygame.transform.scale(road_surface, (1000,600))
+road_surface = pygame.transform.scale(road_surface, (1000, 600))
+ROAD_H = 600
+
+# Player's car, its speed is determined by players typing speed.
 car1_surface = pygame.image.load('assets/car1.png').convert_alpha()
-car1_surface = pygame.transform.scale(car1_surface, (100,100))    
-car1_rect = car1_surface.get_rect(center = (545,470))
+car1_surface = pygame.transform.scale(car1_surface, (100, 100))
+car1_rect = car1_surface.get_rect(center=(545, 470))
+
 car2_surface = pygame.image.load('assets/car2.png').convert_alpha()
-car2_surface = pygame.transform.scale(car2_surface, (100,100))    
-car2_rect = car1_surface.get_rect(center = (470,470))
+car2_surface = pygame.transform.scale(car2_surface, (100, 100))
+car2_rect = car2_surface.get_rect(center=(470, 470)) 
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
 
-    screen.blit(road_surface, (0,0))
+    scroll += car_speed
+    if scroll >= ROAD_H:
+        scroll -= ROAD_H
+
+    screen.blit(road_surface, (0, scroll))
+    screen.blit(road_surface, (0, scroll - ROAD_H))
+
     screen.blit(car1_surface, car1_rect)
     screen.blit(car2_surface, car2_rect)
+
     pygame.display.update()
     clock.tick(60)
