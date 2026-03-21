0454a9d883e65673414454399590fc131a341c2b
KhanSayeem <khansayeem03@gmail.com>
2026-03-22 03:24:10 +0600

    Fix screen render issue, need to separate user car speed from computer's

--- mvp.py
+++ mvp.py
@@ -26,17 +26,17 @@ text_surface = font.render(target_text, True, (28, 26, 1))
 typing_start_time = None
 last_typed_time = None
 wpm = 0
-decrease_speed_after_interval = 1.5 # cars speed will slow down after this much time of inactivity
-decrease_speed_rate = 0.8
+decrease_speed_after_interval = 0.25 # cars speed will slow down after this much time of inactivity
+decrease_speed_rate = 0.88
 
 def calculate_wpm(start_time, char_typed):
-    elapsed_mins = (time.time() - start_time)
+    elapsed_mins = (time.time() - start_time) / 60
     if elapsed_mins == 0:
         return 0
     words_typed = char_typed / 5 # 5 characters = 1 word
     return words_typed / elapsed_mins
 
-def wpm_to_speed(wpm): return wpm/1 # The entire game is 60fps, if the return raw wpm as speed then the car will go out of screen
+def wpm_to_speed(wpm): return wpm/10 # The entire game is 60fps, if the return raw wpm as speed then the car will go out of screen
 
 
 # Player's car, its speed is determined by players typing speed.
@@ -52,49 +52,7 @@ correct_color = (0, 255, 0)
 incorrect_color = (255,0,0)
 incomplete_color = (28, 26, 1)
 
-
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            exit()
-        
-        if event.type == pygame.TEXTINPUT:
-            if len(user_input) < len(target_text):
-                if typing_start_time is None:
-                    typing_start_time = time.time()
-                user_input += event.text
-                last_typed_time = time.time()
-
-                wpm = calculate_wpm (typing_start_time, len(user_input))
-
-
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_BACKSPACE:
-                user_input = user_input[:-1]
-                last_typed_time = time.time()
-    
-    if last_typed_time is not None:
-        idle_time = time.time() - last_typed_time 
-        if idle_time > decrease_speed_after_interval:
-            wpm *= decrease_speed_rate
-    
-    car_speed = wpm_to_speed(wpm)
-
-    scroll += car_speed
-    if scroll >= road_height:
-        scroll -= road_height
-
-    screen.blit(road_surface, (0, scroll))
-    screen.blit(road_surface, (0, scroll - road_height))
-
-    screen.blit(car1_surface, car1_rect)
-    screen.blit(car2_surface, car2_rect)
-
-    screen.blit(text_bg, text_bg_rect)
-    # screen.blit(text_surface, (130, 50))
-
-    def validate_text():
+def validate_text():
             current_char = 110
             for i, char in enumerate(target_text):
                 if i < len(user_input):
@@ -110,6 +68,54 @@ while True:
                 char_surface = font.render(char, True, color)   
                 screen.blit(char_surface, (current_char, 50)) 
                 current_char += char_surface.get_width()         
+    
+
+            
+
+while True:
+    screen.fill((0,0,0))
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+        
+        if event.type == pygame.TEXTINPUT:
+            if len(user_input) < len(target_text):
+                if typing_start_time is None:
+                    typing_start_time = time.time()
+                user_input += event.text
+                last_typed_time = time.time()
+
+                wpm = calculate_wpm (typing_start_time, len(user_input))
+                
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_BACKSPACE:
+                user_input = user_input[:-1]
+                last_typed_time = time.time()
+                    
+    if last_typed_time is not None:
+        idle_time = time.time() - last_typed_time 
+        if idle_time > decrease_speed_after_interval:
+            wpm *= decrease_speed_rate
+        
+    car_speed = wpm_to_speed(wpm)
+    
+    scroll += car_speed
+    if scroll >= road_height:
+        scroll -= road_height
+
+    
+    scroll = (scroll + car_speed) % road_height # counter to keep inside 600, 
+                                                # When (scroll + car_speed) = 600, (% road_height) resets to 0
+    screen.blit(road_surface, (0, scroll - road_height))
+    screen.blit(road_surface, (0, scroll))
+
+    screen.blit(car1_surface, car1_rect)
+    screen.blit(car2_surface, car2_rect)
+
+    screen.blit(text_bg, text_bg_rect)
+
     
     validate_text()
     wpm_surface = font.render(f"WPM: {int(wpm)}", True, (255,255,255))
